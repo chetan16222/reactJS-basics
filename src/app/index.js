1be0848f46772d2424060da23d9cc6ea36bbf845
chetan16222 <chetan16222@gmail.com>
2017-07-05 15:39:53 -0700

    changes

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,13 @@
-console.log("It works");
+import React from "react";   //adding dependacy to component
+import ReactDOM from "react-dom"; 
+
+// create a component App is our root component
+// component have a lots builtin method render is one of them
+// after that how to run that component  go to index.html setup a hook
+class App extends React.Component{
+    render() {
+        return ();
+    }
+}
+
+ReactDOM.render(<App/>,window.document.getElementById("app"));
